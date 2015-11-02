ed6430a36e0d3774e797360beac5aefc71bba4a1
Pedro Dias <up201404178@fe.up.pt>
2015-11-02 22:17:11 +0000

    Revert "prova"
    This reverts commit b1ded59681250807bae95381b321c4f73030446e.

--- src/Prova.h
+++ src/Prova.h
@@ -1,84 +1,27 @@
 #ifndef PROVA_H
 #define PROVA_H
 
-#include <string>
-
-#include "atleta.h"
-#include "Calendario.h"
-
-class Posicao{
-private:
-	Atleta * atleta;
-public:
-	Posicao(Atleta * atleta);
-	Atleta *getAtleta() const;
-};
-class Posicao_tempo : public Posicao{
-private:
-	Data tempo;
-public:
-	Posicao_tempo(Atleta * atleta , Data tempo);
-	Data getTempo() const;
-	void setTempo(Data tempo);
-	 bool operator < (const Posicao_tempo &A) const;
-};
-
-class Posicao_Pontos : public Posicao{
-private:
-	int pontuacao;
-public:
-	Posicao_Pontos(Atleta * atleta, int pontuacao);
-	int getPontuacao() const;
-	void setpontuacao(int pontuacao);
-	bool operator < (const Posicao_Pontos &A) const;
-};
 
 class Prova{
-	string Nome;
+
+
+string Nome;
+//FALTA COMO GUARDAR A PROVA;
 
 public:
-	//Criar
-	Prova(string Nome);
 
-	//Gerir
-	void setNome(string nome);
-	virtual void ordena();
-
-	//Ler
-	string getNome()const;
+virtual void pontuacao();
 };
-
-
-class Prova_Pontuacao : public Prova {
-private:
-	vector<Posicao_Pontos *> lugares;
+class Prova_HIGH_SCORE : public Prova {
 public:
-	void ordena();
-	void adicionaLugar(Posicao_Pontos *lugar);
-	void removeLugar(Posicao_Pontos *lugar);
-	vector<Posicao_Pontos *> getLugares() const;
+void pontuacao();
 
 };
 
 
-class Prova_Tempo: public Prova{
-private:
-	vector<Posicao_tempo *> lugares;
+class Prova_TIMER: public Prova{
 public:
-	void ordena();
-	void adicionaLugar(Posicao_tempo *lugar);
-	void removeLugar(Posicao_tempo *lugar);
-	vector<Posicao_tempo *> getLugares() const;
-
-};
-
-class lugarRepetida{
-public:
-	lugarRepetida(){};
-};
-class lugarNaoExiste{
-public:
-	lugarNaoExiste(){};
+void pontuacao();
 };
 #endif
 

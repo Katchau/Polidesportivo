8b1c1da63ee4b60dc76c27fd1d189ce93087e074
DiogoMCampos <d.diogocampos@gmail.com>
2015-12-22 03:39:18 +0000

    Revert "Trabalho Entregye"
    This reverts commit 6672994f09e88c773746b9c0f78c73be84e0969b.

--- src/Calendario.h
+++ src/Calendario.h
@@ -305,10 +305,7 @@ public:
 	 *
 	 */
 	void ordena();
-	/**
-    * Adiciona a classificacao do atleta na prova
-	* @param nomeAteta nome do Atleta
-    */
+
 	void adicionaLugar(string nomeAtleta,int d, int m , int a, int h, int mi, int s, int score); //TODOalgumas duvidas
 	/**
 	 * Remove a classificacao do atleta na prova
